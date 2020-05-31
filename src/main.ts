ab0c6f5d8e9f24ced9e86168d9a73c1d46d1f3dc
Audun Halland <audun.halland@gmail.com>
2020-05-31 14:18:46 +0200

    Example query with connection pool, transaction and sql template tag

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,70 @@
+import { Pool, ClientBase, PoolClient } from 'pg';
 import sql, { Sql } from 'sql-template-tag';
 
-const testQuery = (id?: number): Sql => {
+// Some sql constants
+const TRUE = sql`TRUE`;
+
+// Client wrapper for forcing the use of the Sql type
+class PgContext {
+  private client: ClientBase;
+
+  constructor(client: ClientBase) {
+    this.client = client;
+  }
+
+  async query<T>(sql: Sql): Promise<Array<T>> {
+    const result = await this.client.query(
+      sql.text,
+      sql.values,
+    );
+    return result.rows;
+  }
+}
+
+async function transaction<T>(
+  exec: (context: PgContext) => Promise<T>,
+  pool: Pool
+): Promise<T> {
+  // Acquire available connection from pool
+  const client = await pool.connect();
+  try {
+    await client.query('BEGIN');
+    const context = new PgContext(client);
+    const value =  await exec(context);
+    await client.query('COMMIT');
+    client.release();
+    return value;
+  } catch (err) {
+    client.release(err);
+    throw err;
+  }
+}
+
+const exampleQuery = async (
+  ctx: PgContext,
+  { id }: {
+    id?: number
+  }
+): Promise<any> => {
   const query = sql`
     SELECT *
-    FROM yo
+    FROM foo
     WHERE
-      ${id ? sql`id = ${id}` : sql`TRUE`}
+      ${id ? sql`id = ${id}` : TRUE}
   `;
 
-  return query;
+  console.log(query.text);
+
+  return await ctx.query(query);
 }
 
 async function main() {
-  console.log(testQuery(32).text);
-  console.log(testQuery().text);
+  const pool = new Pool();
+
+  await transaction(async ctx => {
+    await exampleQuery(ctx, { id: 32 });
+    await exampleQuery(ctx, {});
+  }, pool);
 }
 
 main();
